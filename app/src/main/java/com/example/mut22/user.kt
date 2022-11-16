7fefa12cae2ce9793a0962bde22f06f52dc1cb57
norhidayah <dwiranii>
2022-11-16 09:13:22 +0700

    first commit

---
+++ app/src/main/java/com/example/mut22/user.kt
@@ -0,0 +1,11 @@
+package com.example.mut22
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class user : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+    }
+}
